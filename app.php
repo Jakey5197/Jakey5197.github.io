de4f579d73a793ac40a1ca5806e0b032c50d7423
jacob <104648750+Jakey5197@users.noreply.github.com>
2024-05-05 21:36:35 -0500

    asd

--- app.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // Get the visitor's IP address
-    $ip = $_SERVER['REMOTE_ADDR'];
-
-    // Save the IP address to data.txt file
-    $file = fopen('data.txt', 'a');
-    fwrite($file, $ip . "\n");
-    fclose($file);
-?>
