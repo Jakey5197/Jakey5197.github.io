24c642f5e78d4e00a19cea53db5ef56293b31b56
jacob <104648750+Jakey5197@users.noreply.github.com>
2024-05-05 20:45:42 -0500

    a

--- app.php
+++ app.php
@@ -1,3 +1,13 @@
 <?php
+    $user_ip = $_SERVER['REMOTE_ADDR'];
+
+    $file = fopen('data.txt', 'a');
+
+    fwrite($file, $user_ip . "\n");
+
+    fclose($file);
+
+    header('Location: index.html');
     
+    exit;
 ?>
